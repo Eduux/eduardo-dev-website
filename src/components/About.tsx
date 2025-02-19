de7f5d5c27983719f2276738e538158b818939ac
Eduardo Quadros <eduardosantanaquadros@gmail.com>
2025-02-19 09:09:57 -0300

    feat: my image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function About() {
   return (
@@ -15,16 +16,27 @@ export default function About() {
         <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center">
           About Me
         </h2>
-        <p className="text-lg mb-6 text-gray-300">
-          Software Engineer with 8+ years of experience in web development and
-          software engineering. Proven track record of delivering impactful
-          solutions across industries such as banking, financial services,
-          e-commerce, and marketplaces.
-        </p>
-        <p className="text-lg text-gray-300">
-          Passionate about building high-performance, user-friendly applications
-          that enhance user experience and business efficiency.
-        </p>
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-4">
+          <Image
+            className="rounded-full"
+            src="/eduardo.jpg"
+            alt="Profile"
+            width={100}
+            height={100}
+          />
+          <div>
+            <p className="text-lg mb-6 text-gray-300">
+              Software Engineer with 8+ years of experience in web development
+              and software engineering. Proven track record of delivering
+              impactful solutions across industries such as banking, financial
+              services, e-commerce, and marketplaces.
+            </p>
+            <p className="text-lg text-gray-300">
+              Passionate about building high-performance, user-friendly
+              applications that enhance user experience and business efficiency.
+            </p>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
